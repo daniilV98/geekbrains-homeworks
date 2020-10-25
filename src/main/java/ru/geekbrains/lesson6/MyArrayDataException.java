1bc0c8b29f01e2c2981ec4cc096a1e8433309178
RaNgO <daniilverin1125@gmail.com>
2020-10-25 18:15:23 +0300

    homework_6

--- src/main/java/ru/geekbrains/lesson6/MyArrayDataException.java
+++ src/main/java/ru/geekbrains/lesson6/MyArrayDataException.java
@@ -3,7 +3,6 @@ package ru.geekbrains.lesson6;
 public class MyArrayDataException extends Exception {
 
     public MyArrayDataException(String error) {
-
         super(error);
     }
 }
--- src/main/java/ru/geekbrains/lesson6/MyArraySizeException.java
+++ src/main/java/ru/geekbrains/lesson6/MyArraySizeException.java
@@ -3,7 +3,6 @@ package ru.geekbrains.lesson6;
 public class MyArraySizeException extends Exception {
 
     public MyArraySizeException(String error) {
-
         super(error);
     }
 }
